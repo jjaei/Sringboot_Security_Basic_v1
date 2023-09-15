ea1affaba1084f8786a34a9ecbd663f5e177e272
jjaei <ieajnowk@gmail.com>
2023-09-16 00:06:11 +0900

    User entity 생성

---
+++ src/main/java/com/cos/security1/entity/User.java
@@ -0,0 +1,29 @@
+package com.cos.security1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role; // USER, ADMIN
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
