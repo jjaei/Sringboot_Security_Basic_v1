6c0578ed8acde3b6d12de2b733de0b11524c4cb4
jjaei <ieajnowk@gmail.com>
2023-09-16 04:51:47 +0900

    접근 권한 부여

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+// secured 애노테이션 활성화, preAuthorize 애노테이션 활성화
 public class SecurityConfig {
 
     @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌.
