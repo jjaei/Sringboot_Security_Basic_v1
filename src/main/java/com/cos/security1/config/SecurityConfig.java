6c9e31e3bc847c3db1e6df98c96f583deec144d8
jjaei <ieajnowk@gmail.com>
2023-09-16 00:06:05 +0900

    패스워드 인코딩 설정 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
 public class SecurityConfig {
+
+    @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌.
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -20,7 +26,7 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login"); // 로그인 페이지로 이동
+                .loginPage("/loginForm"); // 로그인 페이지로 이동
         return http.build();
 
     }
