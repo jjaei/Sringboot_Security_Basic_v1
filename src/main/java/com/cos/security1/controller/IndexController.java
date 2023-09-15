05bdeb68a45c347c83e86f8ad1b700dba3019642
jjaei <ieajnowk@gmail.com>
2023-09-15 23:08:57 +0900

    컨트롤러 생성

---
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.cos.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소를 낚아챔. -> SecurityConfig 파일 생성 후 낚아채지 않음.
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입이 완료되었습니다.";
+    }
+}
